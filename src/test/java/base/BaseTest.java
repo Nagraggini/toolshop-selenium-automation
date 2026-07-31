48fe096f2bcdc8086a3dc314325f9b3e63cb08d8
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-07-31 23:54:40 +0200

    Revert "feat: set test data class for create test user"
    This reverts commit 8570ea58abb05967a14e288eefd30ff75bc5a8b4.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import driver.DriverFactory;
 import pages.HomePage;
-import utils.TestData;
 
 public class BaseTest {
 
@@ -23,7 +22,6 @@ public class BaseTest {
 	protected static final Logger logger = LogManager.getLogger(BaseTest.class);
 	
 	protected HomePage homePage;
-	protected TestData testData=new TestData();
 
 	@BeforeEach
 	void setUp() {		
